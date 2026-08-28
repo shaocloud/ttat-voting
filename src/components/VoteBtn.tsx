b400d8f58ea7bc25186da4ebfe2b5423e2c73c8f
shao <shaoyun@kpppack.local>
2026-08-28 17:55:06 +0800

    feat: adjust colors and style

--- src/components/VoteBtn.tsx
+++ src/components/VoteBtn.tsx
@@ -6,36 +6,64 @@ interface VoteBtnProps
 }
 
 export function VoteBtn({ info }: VoteBtnProps) {
+    function cardSuit(style : string){
+        let suit = info.card[1];
+        const colDict = {
+            "♥": "text-red-600",
+            "♠": "text-blue-800",
+            "♦": "text-red-400",
+            "♣": "text-green-900",
+        }
+        if(suit.includes("❤"))
+        {
+            suit = `♥`
+        }
+        return (
+            <div className={`
+                flex flex-col 
+                items-center leading-[0.9]
+                text-4xl
+                font-bold 
+                text-black
+                ${colDict[suit]}
+                absolute 
+                ${style}`}>  
+                <span>{info.card[0]}</span>
+                <span>{suit}</span>
+            </div>
+        )
+    }
+
     return (
         <div
             className="
             flex flex-col
             w-60 
-            rounded-md 
-            p-4
-            text-gray-950
+            h-84
+            rounded-lg
+            inset-ring-16
+            inset-ring-white/70
             bg-gray-100
+            bg-cover
             shadow-md
-            select-none
+
             relative"
+            style="
+            background-image: url('./assets/default.png')"
             >
-            <div className="text-xl font-bold absolute top-2 left-2">{info.card}</div>
-            <div className="text-xl font-bold absolute bottom-2 right-2 rotate-180">{info.card}</div>
-            <img
-                src='./assets/default.png'
-                alt={info.name}
-                className="
-                w-9/10
-                object-cover
-                rounded
-                m-auto
-                p-2"
-            />
+            {cardSuit("top-2 left-2")}
+            {cardSuit("bottom-2 right-2 rotate-180")}
             <div
                 className="
-                ">
-                <div>{info.name}</div>
-                <div className="text-xs min-h-4">{info.desc}</div>
+                    font-[vcr]
+                    font-semibold
+                    text-white
+                    text-shadow-ctr
+                    absolute
+                    bottom-5
+                    left-5">
+                <div className="text-xl min-h-4">{info.name}</div>
+                <div className="text-xs min-h-4">{info.desc?.replaceAll('"','')}</div>
             </div>
         </div>
     )
